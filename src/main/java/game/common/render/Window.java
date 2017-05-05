f5940cff0a566cb6cbbeb2f519884001d4bd2392
Zachary Waldron <compsci@compscis-13.local>
2017-05-05 08:56:05 -0700

    Added window rendering

--- src/main/java/game/common/render/Window.java
+++ src/main/java/game/common/render/Window.java
@@ -16,16 +16,22 @@ import static org.lwjgl.system.MemoryUtil.*;
 public class Window implements Runnable {
 
 	private long window;
-	private int width, height;
+	private int width = 0, height = 0;
 	private String title = "SCHTY RPG";
 	
-	private long monitor = glfwGetPrimaryMonitor();
-	
 	public Window() throws NullPointerException {
+		GLFWErrorCallback.createPrint(System.err).set();
+		
+		this.width = width==0?300:width;
+		this.height = height==0?300:height;
 		
 		if (!glfwInit()) throw new NullPointerException();
 		
-		this.window = glfwCreateWindow(width, height, title, monitor, NULL);
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+		
+		this.window = glfwCreateWindow(width, height, title, NULL, NULL);
 		
 		if (this.window == NULL) {
 			glfwTerminate();
@@ -33,8 +39,12 @@ public class Window implements Runnable {
 		}
 		
 		glfwMakeContextCurrent(window);
-		
-		
+	}
+	
+	public Window(int width, int height) throws NullPointerException {
+		this();
+		this.width = width;
+		this.height = height;
 	}
 	
 	private void render() {
@@ -42,6 +52,37 @@ public class Window implements Runnable {
 	}
 	
 	private void update() {
+		
+	}
+	
+	private void init() {
+		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+			
+		});
+		
+		// Using example stack buffer
+		try (MemoryStack stack = stackPush()) {
+			IntBuffer pWidth = stack.mallocInt(1); // int*
+			IntBuffer pHeight = stack.mallocInt(1); // int*
+
+			// Get the window size passed to glfwCreateWindow
+			glfwGetWindowSize(window, pWidth, pHeight);
+
+			// Get the resolution of the primary monitor
+			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+			// Center the window
+			glfwSetWindowPos(window, (vidmode.width() - pWidth.get(0)) / 2, (vidmode.height() - pHeight.get(0)) / 2);
+		}
+		
+		glfwMakeContextCurrent(window);
+		
+		// Synchronize frames
+		glfwSwapInterval(1);
+		glfwShowWindow(window);
+	}
+	
+	public void loop() {
 		
 	}
 
@@ -53,7 +94,8 @@ public class Window implements Runnable {
 	}
 	
 	public void run() {
-		
+		// finishes init
+		init();
 	}
 
 }
